fa34bdb7e62a8b5598698a9fc73b2419fce0251c
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2023-08-19 19:58:52 -0300

    adding the post verb

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,13 +1,14 @@
 package com.example.cardapio.controller;
 
+import com.example.cardapio.dto.FoodRequestDto;
 import com.example.cardapio.dto.FoodResponseDTO;
+import com.example.cardapio.entity.Food;
 import com.example.cardapio.repository.FoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,6 +19,7 @@ public class FoodController {
     @Autowired
     private FoodRepository foodRepository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public ResponseEntity<List<FoodResponseDTO>> getAllFoods() {
         List<FoodResponseDTO> foodResponseDTOList = foodRepository.findAll()
@@ -26,5 +28,17 @@ public class FoodController {
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(foodResponseDTOList);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public ResponseEntity<FoodResponseDTO> saveFood(@RequestBody FoodRequestDto foodRequestDto) {
+        Food food = new Food(foodRequestDto);
+        foodRepository.save(food);
+
+        FoodResponseDTO foodResponseDTO = new FoodResponseDTO(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+
+        return ResponseEntity.created(URI.create("/food/" + food.getId()))
+                .body(foodResponseDTO);
     }
 }
---
+++ src/main/java/com/example/cardapio/dto/FoodRequestDto.java
@@ -0,0 +1,2 @@
+package com.example.cardapio.controller;public record FoodRequestDto() {
+}
